f5cd8c53a1f6cbbb171aa6532c7a3eca8d0ec88e
fujipooo <majesty1579@yahoo.co.jp>
2019-08-17 13:47:32 +0900

    reply機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.create(text:comment_params[:text],user_id: current_user.id,tweet_id: comment_params[:tweet_id])
+    @comment = Comment.create(text:comment_params[:text],user_id: current_user.id,tweet_id: comment_params[:tweet_id],parent_id:comment_params[:parent_id])
     respond_to do |format|
       format.html { redirect_to "/tweets/#{comment_params[:tweet_id]}"}
       format.json
@@ -11,6 +11,6 @@ class CommentsController < ApplicationController
   
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(tweet_id: params[:tweet_id], user_id: current_user.id)
+    params.require(:comment).permit(:text,:parent_id).merge(tweet_id: params[:tweet_id], user_id: current_user.id)
   end
 end
